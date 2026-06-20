f6e4c943c08deaf5d5db0be7d2b9137ce22c6138
Dietrich Daroch <Dietrich@Daroch.me>
2026-06-20 11:38:27 -0400

    Haskell: Add more prettify symbols

--- languages/haskell.el
+++ languages/haskell.el
@@ -3,7 +3,12 @@
 (add-hook 'haskell-mode-hook
           (lambda ()
             (setq prettify-symbols-alist '(
+                                           ;; Numbers
+                                           ("Integer" . ?ℤ)  ;; DOUBLE-STRUCK CAPITAL Z
+                                           ("Natural" . ?ℕ)  ;; DOUBLE-STRUCK CAPITAL N
+
                                            ;; Logic
+                                           (" . "  . ?⋅)  ;; DOT OPERATOR
                                            ("True"  . ?⟙)  ;; LARGE DOWN TACK
                                            ("False" . ?⟘)  ;; LARGE UP TACK
                                            )
