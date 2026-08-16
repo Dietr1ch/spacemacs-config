0d000e46e0987e47e39fee34518f1857b64b7966
Dietrich Daroch <Dietrich@Daroch.me>
2026-08-16 14:57:52 -0400

    agent-shell: Use simpler text header

--- spacemacs/layers.el
+++ spacemacs/layers.el
@@ -142,6 +142,7 @@
 		minuet-model "qwen2.5-coder:3b"
 		minuet-n-completions 3
 		minuet-context-window 512
+		agent-shell-header-style 'text
 		agent-shell-org-transcript-directory "~/Notes/Transcripts/AI/agent-shell/"
 		agent-shell-preferred-agent-config '(preselect . opencode)
 		)
